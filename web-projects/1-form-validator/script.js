a1a491925b58997f116ca6f8c335708f39d4b327
KeisukeShimokawa <shimoke4869@gmail.com>
2020-05-10 15:39:12 +0900

    start form-validator

---
+++ web-projects/1-form-validator/script.js
@@ -0,0 +1,11 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    console.log("submit");
+})
