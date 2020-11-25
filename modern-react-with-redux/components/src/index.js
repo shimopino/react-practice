34e0fc70217bac4ad5570234834c93ef2010f850
KeisukeShimokawa <shimoke4869@gmail.com>
2020-11-25 15:35:36 +0000

    finish components basics

--- modern-react-with-redux/components/src/index.js
+++ modern-react-with-redux/components/src/index.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+// npm install --save faker
+// import faker from 'faker';
+
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
+      {/* <ApprovalCard /> */}
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          content="Nice Blog Post!"
+          avator={"https://source.unsplash.com/random"}
+        />
+      </ApprovalCard>
+
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00AM"
+        content="I like the subject"
+        avator={"https://source.unsplash.com/random"}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 5:45PM"
+        content="I Like the content"
+        avator={"https://source.unsplash.com/random"}
+      />
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
