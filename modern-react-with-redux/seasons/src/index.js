3d81bc5ac7cac9d4e5abfd483f76009e664b8830
KeisukeShimokawa <shimoke4869@gmail.com>
2020-11-27 14:25:31 +0000

    add SeasonDisplay Component

--- modern-react-with-redux/seasons/src/index.js
+++ modern-react-with-redux/seasons/src/index.js
@@ -1,48 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-// const App = () => {
-//   navigator.geolocation.getCurrentPosition(
-//     // 位置情報を表示するコールバック
-//     (position) => {
-//       console.log(position.coords.latitude);
-//       console.log(position.coords.longitude);
-//     },
-//     // エラー情報を表示するコールバック
-//     (err) => console.log(err)
-//   );
-
-//   return <div>Latitude: </div>;
-// };
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
 
-  constructor(props) {
-    super(props);
+  state = { lat: null, errorMessage: '' };
 
-    this.state = { lat: null, errorMessage: '' };
-
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: [position.coords.latitude ]}),
+      err => this.setState({ errorMessage: err.message })
     );
   }
 
-  // React says we have to define render
   render() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     };
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />
     };
 
-    return <div>loading!</div>
+    return <div>loading!</div>;
   }
 }
 
