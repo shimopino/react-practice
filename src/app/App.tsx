a2949f6ab93e23be9b17688f7854a0539417da27
shimopino <shimoke4869@gmail.com>
2022-12-24 07:41:39 +0000

    useStateやuseEffectはレンダリングすらできないことの確認

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,11 @@
+import { Clock } from "./Clock.js";
 import { Page } from "./Page.js";
 
 export const App = () => {
   return (
     <Page>
       <p>Hello, world!</p>
+      <Clock />
     </Page>
   );
 };
